04d9f9973384d69a076c1bbf63be40cf5debab01
domhienxl <domhienxl@gmail.com>
2023-06-20 22:41:03 +0700

    1012-Events

--- firstapp/src/Xe.js
+++ firstapp/src/Xe.js
@@ -8,17 +8,49 @@ class Xe extends React.Component{
             color: props.color,
             value: props.value
         };
+        console.log('this inner constructor:',this);
+        // this.shoot = this.shoot.bind(this);
     }
 
+    shoot(b){
+        // alert('Great Shot!');
+        console.log('shoot this = ', this);
+        console.log('shoot b = ', b);
+    }
+    shoot2 = (a) =>{
+        // console.log('shoot2 this = ', this);
+        console.log('shoot2 a = ', a);
+    }
     // static getDerivedStateFromProps(props,state){
     //     console.log('getDerivedStateFromProps');
     //     return {color: props.color};
     // }
-
+shoot3 = (a,b) => {
+    alert(b.type);
+}
     render(){
-        return (
-        <h1 style={{ color:this.state.color}}>Hello, {this.state.value}</h1>
-        );
+        const shoot = (
+
+            <h1 
+            onClick={this.shoot.bind(this,'Goal shoot 1')} 
+            style={{ color:this.state.color, cursor:'pointer'}}>Hello, {this.state.value}
+            </h1>
+            );
+            const shoot2 = (
+
+                <h1 
+                onClick={() => this.shoot2('Goal')} 
+                style={{ color:this.state.color, cursor:'pointer'}}>Hello, {this.state.value}
+                </h1>
+                );
+                const shoot3 = (
+
+                    <h1 
+                    onClick={(ev) => this.shoot3("Goal", ev)} 
+                    style={{ color:this.state.color, cursor:'pointer'}}>Hello, {this.state.value}
+                    </h1>
+                    );
+        return shoot3;
     }
     componentDidMount(){
         console.log('componentDidMount');
